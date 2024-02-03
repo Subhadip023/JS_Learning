416b596950817b90a4620ab0d705ef6dbf8725cf
Subhadip023 <subhadip240420@gmail.com>
2024-02-03 10:13:04 +0530

    operaters

---
+++ Codes/06_Operators&Expressions.js
@@ -0,0 +1,29 @@
+// Expersion and Codition 
+console.log("Operater in js ...")
+// Arithmatic Operater 
+// let b=4;
+// let a=45;
+// console.log("a+b = " ,a+b)
+// console.log("a-b = " ,a-b)
+// console.log("a/b = " ,a/b)
+// console.log("a*b = " ,a*b)
+// console.log("a**b = " ,a**b)
+// console.log("a%b = " ,a%b)
+// console.log("++a = " ,++a)
+// console.log("--a = " ,--a)
+
+// // Assignment Operaters 
+// let assignment=1; 
+// assignment+=5// same as a=a+5
+// console.log(assignment)
+
+//logical operater 
+let x=5;
+let y=6;
+console.log(x<y&&x==5)//logical and 
+console.log(x>y||x==5)//logical or 
+console.log(!false)//logical not 
+
+// "//" this used for single line comment and '/*....*/' used for multi line comment 
+
+
