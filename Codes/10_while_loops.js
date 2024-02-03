add59ba56914d4ee1151cd41f5e3bc2e62450b45
Subhadip023 <subhadip240420@gmail.com>
2024-02-03 14:37:20 +0530

    function

---
+++ Codes/10_while_loops.js
@@ -0,0 +1,15 @@
+// While loop 
+// console.log("While loop ---->>> ");
+// let i=0;
+// while(i<10){
+//     console.log(i);
+//     i++;
+// }
+
+// Do While Loop 
+console.log("Do While loop ---->>> ");
+let i=0;
+do{
+    console.log(i);
+    i++;
+}while(i<10)
---
+++ Codes/11_function.js
@@ -0,0 +1,7 @@
+// Function in js 
+
+function sum(a,b) {
+    return a+b;
+}
+console.log("the sum of 4 and 5 is ",sum(4,5))
+console.log("the avgerage of 4 and 5 is ",sum(4,5)/2)
