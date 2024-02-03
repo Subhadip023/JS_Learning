1447f1dc3d3c72b5d7d13d2e9273be09379e0557
Subhadip023 <subhadip240420@gmail.com>
2024-02-03 20:48:50 +0530

    Your commit message

--- Codes/12_function_and_loop_practise_set.js
+++ Codes/12_function_and_loop_practise_set.js
@@ -1,2 +1,12 @@
 //problem 1
-const 
+const studentNumber={
+    subhadip :10,
+    harry: 20,
+    ocean:40,
+    vai:30,
+    burao:70
+}
+
+for(i in studentNumber){
+    console.log(i+" Get ",studentNumber[i]+" Number in  exam")
+}
