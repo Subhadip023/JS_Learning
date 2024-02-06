767fa6c0ff6986ce213785f9df716b6d3f9960e7
Subhadip023 <subhadip240420@gmail.com>
2024-02-06 23:52:41 +0530

    Dom BOm and window introduction

---
+++ Codes/27_bom,dom_window_obj.js
@@ -0,0 +1,8 @@
+// Dom  Docuemnt object model 
+
+window.console.log(window)
+console.log(document.body)
+document.body.style.background="greeen";
+
+//  Bom brouser object model like  propmt alert etc ..
+
