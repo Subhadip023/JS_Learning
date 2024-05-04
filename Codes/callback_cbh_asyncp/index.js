fd94020136819a13f801b5c55e8f8889d650cb09
Subhadip023 <subhadip240420@gmail.com>
2024-05-04 07:18:54 +0530

    pasdpfkwe

--- Codes/callback_cbh_asyncp/index.js
+++ Codes/callback_cbh_asyncp/index.js
@@ -36,16 +36,50 @@
 // console.log(calculater(12,6,mul))
 
 // Callback hell
-function getData(dataId,getNextData) {
-  setTimeout(() => {
-    console.log("data", dataId);
-    if (getNextData) {
-      getNextData();
-    }
-  }, 2000);// 2000 menas 2 see 
-}
+// function getData(dataId,getNextData) {
+//   setTimeout(() => {
+//     console.log("data", dataId);
+//     if (getNextData) {
+//       getNextData();
+//     }
+//   }, 2000);// 2000 menas 2 see 
+// }
 
-getData(1,()=>{
-  getData(2);
+// // Callback hell to solved it we use promises
+// getData(1,()=>{
+//   getData(2,()=>{
+//     getData(3,()=>{
+//       getData(4)
+//     })
+//   });
+// });
+
+// Promises
+
+// let promise =new Promise((resolve,reject)=>{
+//   console.log("i am a promise")
+// })
+
+// Promise has 3 steges they are 
+/*
+  1.pending 
+  2.fulfiled (resolved)
+  3.rejected 
+*/
+const getPromise =()=>{
+  return new Promise((resolve,reject)=>{
+  console.log("i am a promise")
+  // resolve("success");
+  reject("some error ocured");
+})}
+
+
+let promise=getPromise();
+promise.then((res)=>{
+  console.log("promise fulfilled: ",res)
 });
+
+promise.catch((err)=>{
+  console.log("some error occured: ",err)
+})
 
